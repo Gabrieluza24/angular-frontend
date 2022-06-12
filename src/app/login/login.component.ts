12a69b7f8b38e7580aa37ac9f51079a7203ec738
Gabrieluza24 <gabrieluza24@gmail.com>
2022-06-11 20:06:34 -0400

    :sparkles: New Feature: Login Module

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  public loginForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {}
+  
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.maxLength(16), Validators.minLength(8)]]
+    });
+  }
+
+  onSubmit(): void {
+    
+  }
+
+}
